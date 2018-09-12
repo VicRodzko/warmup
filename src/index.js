a1826c12a24e3bcdf07518304bbc3490c30ca9bf
Victor Rodzko <vicrodzko@mail.ru>
2018-09-12 10:18:37 +0300

    Translation to Fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 module.exports = function warmup(temperature) {
-  if (temperature === -20) return -4;
-  else if (temperature === 0) return 32;
-  else if (temperature === 15) return 59;
-  else if (temperature === 40) return 104;
+  fahrenheit = temperature * 1.8 + 32;
+  return fahrenheit;
 };
